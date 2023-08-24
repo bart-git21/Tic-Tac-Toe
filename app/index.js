3c0f0f581ffc3641ff3ee2db95be3cdcc7feed1a
gitName <bartk@list.ru>
2023-08-24 18:01:44 +0300

    fix: rewrite the game logic

--- app/index.js
+++ app/index.js
@@ -3,57 +3,66 @@ const resultField = document.querySelector(".resultField");
 const restartBtn = document.querySelector(".restartBtn");
 const boxes = document.querySelectorAll(".box");
 let whichTurn = true;
+let userText = "";
+let gameArray = [];
 const winningCases = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
-let oArray = [];
-let xArray = [];
+let currentWinningCases = [];
 
-restartBtn.addEventListener("click", start);
+function writtenIntoBox(e) {
+  e.target.textContent = userText;
+}
+function pushArray(e) {
+  gameArray[e.target.id] = userText;
+}
+function checking() {
+  currentWinningCases.forEach((el) => {
+    if (el.every((i) => gameArray[i] === userText)) {
+      return (resultField.textContent = `${userText} is winning!`);
+    }
+    const isRawIncludesX = el.some((e) => gameArray[e] === "X");
+    const isRawIncludesO = el.some((e) => gameArray[e] === "O");
+    isRawIncludesX &&
+      isRawIncludesO &&
+      (currentWinningCases = currentWinningCases.filter(
+        (e) => JSON.stringify(e) !== JSON.stringify(el)
+      ));
+  });
+  if (currentWinningCases.length === 1) {
+    const lastClearRawText =
+      gameArray[currentWinningCases[0][0]] ||
+      gameArray[currentWinningCases[0][1]];
+    lastClearRawText === userText && (resultField.textContent = `it's draw`);
+  } else if (!currentWinningCases.length) {
+    resultField.textContent = `it's draw`;
+  }
+}
+function onClick(e) {
+  userText = whichTurn ? "X" : "O";
+  whichTurn = !whichTurn;
+  writtenIntoBox(e);
+  pushArray(e);
+  checking();
+}
 function start() {
-    resultField.textContent = null;
-    boxes.forEach(
-        e => e.textContent = ""
-    )
-    ticTacWrapper.removeEventListener("click", nextStep);
-    ticTacWrapper.addEventListener("click", nextStep);
-    oArray = Array(9).fill(0);
-    xArray = Array(9).fill(0);    
-    whichTurn = true;
+  currentWinningCases = [...winningCases];
+  gameArray = [...Array(9).fill("")];
+  whichTurn = true;
+  resultField.textContent = "";
+
+  boxes.forEach((e, index) => {
+    e.addEventListener("click", onClick);
+    e.id = index;
+    e.textContent = "";
+  });
 }
 start();
-
-
-boxes.forEach(
-    (e, index) => {
-        e.id = index;
-    }
-)
-
-function nextStep(e) {
-    if (!e.target.textContent) {
-        (whichTurn ? xArray : oArray)[e.target.id] = 1;
-        enterTo(e.target);
-        check(whichTurn ? xArray : oArray);
-        whichTurn = !whichTurn;
-    }
-}
-function check(array) {
-    for (let e of winningCases) {
-        if (array[e[0]] === 1 && array[e[1]] === 1 && array[e[2]] === 1) {
-            resultField.textContent = `${whichTurn ? 'X' : 'O'} is win!`;
-            ticTacWrapper.removeEventListener("click", nextStep);
-            return;
-        }
-    }
-}
-function enterTo(div) {
-    div.textContent = whichTurn ? "x" : "o";
-}
+restartBtn.addEventListener("click", start);
