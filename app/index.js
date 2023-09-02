14a5375ab3f6b2208db5ec58a97d345cca8974a2
gitName <bartk@list.ru>
2023-09-02 19:27:40 +0300

    fix: add default state

--- app/index.js
+++ app/index.js
@@ -1,55 +1,57 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
 var resultField = document.querySelector(".result-field");
 var restartBtn = document.querySelector(".restart-btn");
 var boxes = document.querySelectorAll(".box");
-var whichTurn = true;
-var userText = "";
-var gameArray = [];
-var winningCases = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
-var currentWinningCases = [];
-function clickOnBox(e) {
+var defaultValue = {
+    whichTurn: true,
+    text: "",
+    gameArray: new Array(9).fill(""),
+    winningCases: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ],
+};
+var gameValue = {};
+function ontypingBox(e) {
     var target = e.target;
-    target.textContent = userText;
+    target.textContent = gameValue.text;
     target.removeEventListener("click", onClick);
 }
-function pushArray(e) {
-    var targetId = +e.target.id;
-    gameArray[targetId] = userText;
+function updateGameArray(e) {
+    var index = +e.target.id;
+    gameValue.gameArray[index] = gameValue.text;
 }
 function checking() {
-    if (currentWinningCases.length > 1) {
-        currentWinningCases.forEach(function (el) {
-            if (el.every(function (i) { return gameArray[i] === userText; })) {
+    if (gameValue.winningCases.length > 1) {
+        gameValue.winningCases.forEach(function (el) {
+            if (el.every(function (i) { return gameValue.gameArray[i] === gameValue.text; })) {
                 removeOnclick();
-                return (resultField.textContent = "".concat(userText, " is winning!"));
+                return (resultField.textContent = "".concat(gameValue.text, " is winning!"));
             }
-            var isRawIncludesX = el.some(function (e) { return gameArray[e] === "X"; });
-            var isRawIncludesO = el.some(function (e) { return gameArray[e] === "O"; });
-            isRawIncludesX &&
-                isRawIncludesO &&
-                (currentWinningCases = currentWinningCases.filter(function (e) { return JSON.stringify(e) !== JSON.stringify(el); }));
+            removeLosingCases(el);
         });
-        if (currentWinningCases.length === 1) {
-            var lastClearRawText = gameArray[currentWinningCases[0][0]] ||
-                gameArray[currentWinningCases[0][1]];
-            lastClearRawText === userText &&
+        if (gameValue.winningCases.length === 1) {
+            var _a = gameValue.winningCases
+                .flat()
+                .map(function (e) { return gameValue.gameArray[e]; }), text1 = _a[0], text2 = _a[1];
+            var lastRawText = text1 || text2;
+            lastRawText === gameValue.text &&
                 (resultField.textContent = "it's draw") &&
                 removeOnclick();
         }
@@ -59,16 +61,14 @@ function checking() {
     }
 }
 function onClick(e) {
-    userText = whichTurn ? "X" : "O";
-    whichTurn = !whichTurn;
-    clickOnBox(e);
-    pushArray(e);
+    gameValue.text = gameValue.whichTurn ? "X" : "O";
+    gameValue.whichTurn = !gameValue.whichTurn;
+    ontypingBox(e);
+    updateGameArray(e);
     checking();
 }
 function startGame() {
-    currentWinningCases = __spreadArray([], winningCases, true);
-    gameArray = __spreadArray([], Array(9).fill(""), true);
-    whichTurn = true;
+    gameValue = __assign({}, defaultValue);
     resultField.textContent = "";
     boxes.forEach(function (e, index) {
         e.addEventListener("click", onClick);
@@ -80,5 +80,13 @@ startGame();
 restartBtn.addEventListener("click", startGame);
 function removeOnclick() {
     // remove onClick from EMPTY boxes
-    gameArray.forEach(function (e, id) { return !e && boxes[id].removeEventListener("click", onClick); });
+    gameValue.gameArray.forEach(function (e, id) { return !e && boxes[id].removeEventListener("click", onClick); });
+}
+function removeLosingCases(arr) {
+    // get winning cases without losing cases (because of raws are completed)
+    var isRawIncludesX = arr.some(function (e) { return gameValue.gameArray[e] === "X"; });
+    var isRawIncludesO = arr.some(function (e) { return gameValue.gameArray[e] === "O"; });
+    isRawIncludesX &&
+        isRawIncludesO &&
+        (gameValue.winningCases = gameValue.winningCases.filter(function (e) { return JSON.stringify(e) !== JSON.stringify(arr); }));
 }
--- app/index.ts
+++ app/index.ts
@@ -1,52 +1,55 @@
 const resultField = document.querySelector(".result-field") as HTMLDivElement;
 const restartBtn = document.querySelector(".restart-btn") as HTMLDivElement;
 const boxes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".box");
-let whichTurn = true;
-let userText = "";
-let gameArray: string[] = [];
 
-type MultiArrayType = number[][];
-const winningCases: Readonly<MultiArrayType> = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-let currentWinningCases: MultiArrayType = [];
+type DefaultValueType = {
+  whichTurn: boolean;
+  text: string;
+  gameArray: string[];
+  winningCases: Readonly<number[][]>;
+};
+const defaultValue: DefaultValueType = {
+  whichTurn: true,
+  text: "",
+  gameArray: new Array(9).fill(""),
+  winningCases: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+};
+let gameValue: DefaultValueType = {} as DefaultValueType;
 
-function clickOnBox(e: MouseEvent): void {
+function ontypingBox(e: MouseEvent): void {
   const target = e.target as HTMLDivElement;
-  target.textContent = userText;
+  target.textContent = gameValue.text;
   target.removeEventListener("click", onClick);
 }
-function pushArray(e: MouseEvent): void {
-  const targetId: number = +(e.target as HTMLDivElement).id;
-  gameArray[targetId] = userText;
+function updateGameArray(e: MouseEvent): void {
+  const index: number = +(e.target as HTMLDivElement).id;
+  gameValue.gameArray[index] = gameValue.text;
 }
 function checking() {
-  if (currentWinningCases.length > 1) {
-    currentWinningCases.forEach((el) => {
-      if (el.every((i) => gameArray[i] === userText)) {
+  if (gameValue.winningCases.length > 1) {
+    gameValue.winningCases.forEach((el) => {
+      if (el.every((i) => gameValue.gameArray[i] === gameValue.text)) {
         removeOnclick();
-        return (resultField.textContent = `${userText} is winning!`);
+        return (resultField.textContent = `${gameValue.text} is winning!`);
       }
-      const isRawIncludesX = el.some((e) => gameArray[e] === "X");
-      const isRawIncludesO = el.some((e) => gameArray[e] === "O");
-      isRawIncludesX &&
-        isRawIncludesO &&
-        (currentWinningCases = currentWinningCases.filter(
-          (e) => JSON.stringify(e) !== JSON.stringify(el)
-        ));
+      removeLosingCases(el);
     });
-    if (currentWinningCases.length === 1) {
-      const lastClearRawText =
-        gameArray[currentWinningCases[0][0]] ||
-        gameArray[currentWinningCases[0][1]];
-      lastClearRawText === userText &&
+
+    if (gameValue.winningCases.length === 1) {
+      const [text1, text2] = gameValue.winningCases
+        .flat()
+        .map((e) => gameValue.gameArray[e]);
+      const lastRawText = text1 || text2;
+      lastRawText === gameValue.text &&
         (resultField.textContent = `it's draw`) &&
         removeOnclick();
     }
@@ -55,16 +58,14 @@ function checking() {
   }
 }
 function onClick(e: MouseEvent) {
-  userText = whichTurn ? "X" : "O";
-  whichTurn = !whichTurn;
-  clickOnBox(e);
-  pushArray(e);
+  gameValue.text = gameValue.whichTurn ? "X" : "O";
+  gameValue.whichTurn = !gameValue.whichTurn;
+  ontypingBox(e);
+  updateGameArray(e);
   checking();
 }
 function startGame() {
-  currentWinningCases = [...winningCases];
-  gameArray = [...Array(9).fill("")];
-  whichTurn = true;
+  gameValue = { ...defaultValue };
   resultField.textContent = "";
 
   boxes.forEach((e, index) => {
@@ -78,7 +79,22 @@ restartBtn.addEventListener("click", startGame);
 
 function removeOnclick() {
   // remove onClick from EMPTY boxes
-  gameArray.forEach(
+  gameValue.gameArray.forEach(
     (e, id) => !e && boxes[id].removeEventListener("click", onClick)
   );
+}
+function removeLosingCases(arr: number[]): void {
+  // get winning cases without losing cases (because of raws includes both options)
+  const isRawIncludesX = arr.some((e) => gameValue.gameArray[e] === "X");
+  const isRawIncludesO = arr.some((e) => gameValue.gameArray[e] === "O");
+  isRawIncludesX &&
+    isRawIncludesO &&
+    (gameValue.winningCases = gameValue.winningCases.filter(
+      (e) => JSON.stringify(e) !== JSON.stringify(arr)
+    ));
+
+    
+    // ================== TODO
+    // add case when raw includes two X and next value is O
+    // this winningCase should be deleted
 }
